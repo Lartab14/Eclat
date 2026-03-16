92561a56cdd9e47adbfdb2a2702608f5f5a75afd
julianaBE2611 <juli.botina05@gmail.com>
2026-03-16 09:55:59 -0500

    actualizacion login

--- proyectodegrado/src/Login.js
+++ proyectodegrado/src/Login.js
@@ -4,7 +4,7 @@ import './Login.css';
 import LogoEclat from './img/LogoEclat.png';
 
 // Configuración de la URL base del API
-const API_URL = 'https://backend-production-96d5.up.railway.app/api';
+const API_URL = 'https://backend-production-96d5.up.railway.app';
 //const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 export default function Login({ onLoginSuccess }) {
